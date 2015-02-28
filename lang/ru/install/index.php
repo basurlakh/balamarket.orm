e996e91efedb4907416beff770353658a6e5dbaf
Zhelonkin Anton <zhelonkinanton@gmail.com>
2015-02-28 16:24:37 +0300

    Меняем ссылку на партнера

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -2,5 +2,5 @@
 $MESS["balamarket.orm_MODULE_NAME"] = "Модуль для работы с инфоблоками через ORM";
 $MESS["balamarket.orm_MODULE_DESC"] = "";
 $MESS["balamarket.orm_PARTNER_NAME"] = "balamarket.com";
-$MESS["balamarket.orm_PARTNER_URI"] = "http://balamarket.com";
+$MESS["balamarket.orm_PARTNER_URI"] = "https://github.com/ibestrest/balamarket.orm";
 ?>
